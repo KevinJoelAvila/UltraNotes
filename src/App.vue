33ca7540cca69ec7190a9be5986b88a397630fcb
KevinJoelAvila <kevin.arj5@gmail.com>
2021-01-18 16:51:43 +0100

    Creting folders structure

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>LLIBRERIA</h1>
+  <!-- <div id="nav">
+    <router-link to="/">Inici</router-link>
+  </div>
+  <hr />
+  <router-view /> -->
+</template>
+
+<style>
+body {
+  background-color: rgb(173, 31, 255);
+  color: white;
+}
+
+#app {
+  padding: 30px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+#nav a {
+  font-weight: bold;
+  text-decoration: none;
+  color: white;
+}
+
+h1 {
+  font-size: 3em;
+}
+</style>
