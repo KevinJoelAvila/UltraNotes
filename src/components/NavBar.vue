423624ac6c52fc7af6fe571d399dc4d8cc23dc29
New-Era-Studios <blackdemonzdiscord@gmail.com>
2021-01-19 18:45:27 +0100

    Creacion del componente de la barra de navegacion

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,6 @@
 <template>
-    <div id="nav-bar">
-        <header>
-            <div id="logo">
-                <img src="../assets/notas.png" alt="">
-            </div>
-            <h2>Ultranotes</h2>
-            <nav id="menu">
-                <ul>
-                    <li>
-                        <a href=""></a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+    <div id="NavBar">
+
     </div>
 </template>
 
@@ -26,5 +14,22 @@ export default {
 </script>
 
 <style scoped>
+/* Resetear todos los componentes de la web para que se quiten los espacios de los laterales y de arriba, para pegar elementos
+   bien encima de la pantalla y a los laterales */
+* {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    font-size: 100%;
+    vertical-align: baseline;
+    background: transparent;
+}
 
+#NavBar
+{
+    background-color: rgba(192, 27, 233, 0.507);
+    width: 100%;
+    height: 8vh;
+}
 </style>
