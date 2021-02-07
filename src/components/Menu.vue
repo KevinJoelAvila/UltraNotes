a4c692f38485ba2925febaf40ef94ea3aa63665f
KevinJoelAvila <kevin.arj5@gmail.com>
2021-02-07 21:01:35 +0100

    Creando menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,7 +1,24 @@
 <template>
     <div id="menu">
-        <main>
-            
+        
+        <div class="dd">
+            <main>
+                <div id="burger" class="hamburger" @click="myFilter()" :class="{'open':isActive }">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </main>
+            <div class="dd-menu" :class="{'active':isActive }">
+                <ul>
+                    <li>Profile & status</li>
+                    <li>Notification</li>
+                    <li>Help</li>
+                    <li>Log out</li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,13 +27,18 @@ export default {
     name: 'Menu',
     data() {
         return {
-
+            isActive: false
         }
     },
     methods: {
-        
+        myFilter: function() {
+      this.isActive = !this.isActive;
+      // some code to filter users
+    }
     }
 }
+
+
 </script>
 
 <style scoped>
--- src/components/NavBarGeneral.vue
+++ src/components/NavBarGeneral.vue
@@ -43,7 +43,7 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    overflow: hidden;
+    /* overflow: hidden; */
 }
 
 #contenidoCentro
